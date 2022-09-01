a20f91f9d46988ff20471da61c61a1fb1185dc05
Faisal A. F. Rahman <64533786+hann315@users.noreply.github.com>
2022-09-01 16:39:25 +0700

    Change on "disable_web_page_preview" parameter

--- qbittorrent2telegram.sh
+++ qbittorrent2telegram.sh
@@ -14,4 +14,4 @@ ID="YOUR_TELEGRAM_USERID" # To obtain your UID use https://t.me/myidbot
 
 URL="https://api.telegram.org/bot$TOKEN/sendMessage"
 
-curl -S -X POST $URL -d chat_id=$ID -d text="*$3 has finished downloading*%0A%0ATorrent name: $3%0ASize: $6 bytes%0ASave path: $1%0A%0A[Your instance/hostname](Your Domain)" -d parse_mode=Markdown  -d disable_web_page_preview=false> /dev/null 2>&1
+curl -S -X POST $URL -d chat_id=$ID -d text="*$3 has finished downloading*%0A%0ATorrent name: $3%0ASize: $6 bytes%0ASave path: $1%0A%0A[Your instance/hostname](Your Domain)" -d parse_mode=Markdown  -d disable_web_page_preview=true> /dev/null 2>&1
